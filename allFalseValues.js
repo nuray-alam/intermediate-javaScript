e1b9d1631abe75c404a41219d998a6027d6188a7
Md. Nuray Alam Siddik <siddik4x@gmail.com>
2021-02-05 19:09:49 +0600

    testing purspose if else condition added

--- allFalseValues.js
+++ allFalseValues.js
@@ -5,3 +5,10 @@
 // NaN
 
  ////////////////////// all these value are false /////////////////////////////////////////////
+
+ if(){
+     console.log('condition is true');
+ }
+ else{
+     console.log('conditon is false');
+ }
