5ef4794b0dcbc86be925c65141d0aa932e9266f9
Md. Nuray Alam Siddik <siddik4x@gmail.com>
2021-02-13 14:29:52 +0600

    array splice practice file added

---
+++ arraySplice.js
@@ -0,0 +1,4 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const splitedArray = numbers.splice(3, 2); // starting index and count number how many element should be cut off
+console.log(splitedArray);
+console.log(numbers);
