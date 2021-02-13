1ca8ba198cc72bf8ab8510f1a3ed548fe6706941
Md. Nuray Alam Siddik <siddik4x@gmail.com>
2021-02-13 14:32:14 +0600

    array splice with injection practice file added

---
+++ arraySpliceInjected.js
@@ -0,0 +1,5 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const splitedArray = numbers.splice(3, 2,77);
+ // starting index and count number how many element should be cut off(delete count) and after that injected elements
+console.log(splitedArray);
+console.log(numbers);
